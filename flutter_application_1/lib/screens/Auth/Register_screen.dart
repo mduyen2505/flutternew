f761493d7e3933b188cd127ff5c838e953020bd9
duco9t <dung5715@gmail.com>
2024-09-25 08:55:32 +0700

    LoginScene va SignUp scene

--- flutter_application_1/lib/screens/Auth/Register_screen.dart
+++ flutter_application_1/lib/screens/Auth/Register_screen.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class SignUpScene extends StatefulWidget {
+  const SignUpScene({super.key});
+
+  @override
+  State<SignUpScene> createState() => _SignUpSceneState();
+}
+
+class _SignUpSceneState extends State<SignUpScene> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
