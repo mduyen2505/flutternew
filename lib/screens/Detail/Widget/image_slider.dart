372a382b3e95f76edca8f5846051092980de1fa8
duco9t <dung5715@gmail.com>
2024-11-07 13:14:52 +0700

    Fix Layouts Image Detail products

--- lib/screens/Detail/Widget/image_slider.dart
+++ lib/screens/Detail/Widget/image_slider.dart
@@ -13,19 +13,31 @@ class MyImageSlider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 250,
+      height: 250, // Adjust this height as per your design requirement
       child: PageView.builder(
         onPageChanged: onChange,
         itemCount: images.length,
         itemBuilder: (context, index) {
-          return Image.network(
-            images[index],
-            fit: BoxFit.cover,
-            errorBuilder: (context, error, stackTrace) {
-              return const Center(
-                child: Text("Image not available"),
-              );
-            },
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: 16), // Add horizontal padding here
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(
+                  20), // Adjust the radius for rounded corners
+              child: Image.network(
+                images[index],
+                fit: BoxFit.cover, // Make sure the image covers the area
+                width: double
+                    .infinity, // Ensure the image stretches across the container
+                height:
+                    double.infinity, // Ensure the image covers the full height
+                errorBuilder: (context, error, stackTrace) {
+                  return const Center(
+                    child: Text("Image not available"),
+                  );
+                },
+              ),
+            ),
           );
         },
       ),
